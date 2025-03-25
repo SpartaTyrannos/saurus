c3337f90dd55a016eb8e74d66614560137c30dd8
SeongHyeon <sng02006@naver.com>
2025-03-25 17:03:08 +0900

    Games Entity 추, Team Enums 생성

---
+++ src/main/java/com/example/saurus/domain/common/enums/Team.java
@@ -0,0 +1,20 @@
+package com.example.saurus.domain.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Team {
+    SPART_TYRANNOS,
+    SAMSUNG_LIONS,
+    DOOSAN_BEARS,
+    LG_TWINS,
+    SSG_LANDERS,
+    KIA_TIGERS,
+    NC_DINOS,
+    HANWHA_EAGLES,
+    KT_WIZ,
+    LOTTE_GIANTS,
+    KIWOOM_HEROES
+}
