8f30b6698a991d42720375b870aec51812ca48b2
yoorkim <amy0848@naver.com>
2025-03-25 15:40:56 +0900

    chore : init

---
+++ src/main/java/com/example/saurus/SaurusApplication.java
@@ -0,0 +1,13 @@
+package com.example.saurus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaurusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaurusApplication.class, args);
+    }
+
+}
