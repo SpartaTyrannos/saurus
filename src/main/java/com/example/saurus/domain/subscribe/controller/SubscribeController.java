8492cb28cc670a6a9a96350dd50c7ae406a2f288
yoorkim <amy0848@naver.com>
2025-03-26 19:59:46 +0900

    feat : Membership, Subscribe 기능 구현

---
+++ src/main/java/com/example/saurus/domain/subscribe/controller/SubscribeController.java
@@ -0,0 +1,46 @@
+package com.example.saurus.domain.subscribe.controller;
+
+import com.example.saurus.domain.common.annotation.User;
+import com.example.saurus.domain.subscribe.dto.response.SubscribeResponseDto;
+import com.example.saurus.domain.subscribe.service.SubscribeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class SubscribeController {
+
+    private final SubscribeService subscribeService;
+
+    @User
+    @PostMapping("/memberships/{membershipId}/subscribes")
+    public ResponseEntity<String> saveSubscribe(
+            @AuthenticationPrincipal AuthUser authUser,
+            @PathVariable Long membershipId
+    ) {
+        return ResponseEntity.ok(subscribeService.saveSubscribe(authUser, membershipId));
+    }
+
+    @User
+    @GetMapping("/my/subscribes")
+    public ResponseEntity<Page<SubscribeResponseDto>> getSubscribes(
+            @AuthenticationPrincipal AuthUser authUser,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return ResponseEntity.ok(subscribeService.getSubscribes(authUser, page, size));
+    }
+
+    @User
+    @DeleteMapping("/my/subscribes/{subscribeId}")
+    public ResponseEntity<String> deleteSubscribe(
+            @AuthenticationPrincipal AuthUser authUser,
+            @PathVariable Long subscribeId
+    ) {
+        return ResponseEntity.ok(subscribeService.deleteSubscribe(authUser, subscribeId));
+    }
+}
