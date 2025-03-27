a9e957cfbb45b90c684282b0b30448d8e0722f41
yoorkim <amy0848@naver.com>
2025-03-27 11:12:02 +0900

    feat : AOP 추가

---
+++ src/main/java/com/example/saurus/domain/common/aop/AdminAccessAspect.java
@@ -0,0 +1,24 @@
+package com.example.saurus.domain.common.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import com.example.saurus.domain.common.dto.AuthUser;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AdminAccessAspect {
+
+    @Before("@annotation(com.example.saurus.domain.common.annotation.Admin)")
+    public void adminApiAccess(JoinPoint joinPoint) {
+        AuthUser authUser = (AuthUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserRole role = authUser.getUserRole();
+
+        if (role != UserRole.ROLE_ADMIN) {
+            throw new CustomException(HttpStatus.FORBIDDEN, "관리자 권한이 필요합니다.");
+        }
+    }
+}
