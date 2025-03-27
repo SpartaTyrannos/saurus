fd76a93ec668a86d01ffb9a1d66926a8f90c1170
SeongHyeon <sng02006@naver.com>
2025-03-27 13:19:30 +0900

    fix : Team Enums

--- src/main/java/com/example/saurus/domain/game/enums/Teams.java
+++ src/main/java/com/example/saurus/domain/game/enums/Teams.java
@@ -1,19 +1,23 @@
 package com.example.saurus.domain.game.enums;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public enum Teams {
-    SAMSUNG_LIONS,
-    DOOSAN_BEARS,
-    LG_TWINS,
-    SSG_LANDERS,
-    KIA_TIGERS,
-    NC_DINOS,
-    HANWHA_EAGLES,
-    KT_WIZ,
-    LOTTE_GIANTS,
-    KIWOOM_HEROES
+    SAMSUNG_LIONS("삼성 라이온즈"),
+    DOOSAN_BEARS("두산 베어스"),
+    LG_TWINS("엘지 트윈스"),
+    SSG_LANDERS("SSG 랜더스"),
+    KIA_TIGERS("기아 타이거즈"),
+    NC_DINOS("엔씨 다이노스"),
+    HANWHA_EAGLES("한화 이글스"),
+    KT_WIZ("케이티 위즈"),
+    LOTTE_GIANTS("롯데 자이언츠"),
+    KIWOOM_HEROES("키움 히어로즈");
+
+    private final String description;
+
+    Teams(String description) {
+        this.description = description;
+    }
 }
