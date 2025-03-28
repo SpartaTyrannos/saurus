3955e111ec1f1b7b97a4b2074bb438b382392d05
seonghyeon <44752186+sng02006@users.noreply.github.com>
2025-03-28 11:53:48 +0900

    fix : Game Repository findAllByTitleAndDate Query (#30)
    * Games Entity 추, Team Enums 생성
    
    * feat : Game Entity, Teams Enums 생성
    
    * feat : Game Entity, Teams Enums 생성
    
    * Game CRUD
    
    * feat : Game CRUD
    
    * fix : Game CRUD, Teams Enums
    
    * fix : Game Service
    
    * refactor : Game Dtos Rename
    
    * fix : Team Enums
    
    * fix : Game Controller Annotation(Admin), Game Service CustomException
    
    * fix : Game Service Method Parameters
    
    * fix : rm  Game Controller @AuthenticationPrincipal
    
    * fix : Game Save & Update  RequestDto opponent, gameTime, ticketOpen
    
    * chore : 한이님 하셨던 작업으로 다시 변경
    
    * fix : rm not necessary import Dtos
    
    * fix : Game Repository findAllByTitleAndDate Query
    
    * fix : null 체크 추가
    
    ---------
    
    Co-authored-by: OneTwo <102886829+kim-hani@users.noreply.github.com>
    Co-authored-by: yoorkim <amy0848@naver.com>

--- src/main/java/com/example/saurus/domain/game/dto/request/GameUpdateRequestDto.java
+++ src/main/java/com/example/saurus/domain/game/dto/request/GameUpdateRequestDto.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.lang.Nullable;
 
 import java.time.LocalDateTime;
